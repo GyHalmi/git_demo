4e2c960b28d8e076b61c56f80a99dd9d9a0eb7c3
GyHalmi <georgehalmi@gmail.com>
2021-04-08 19:19:56 +0200

    h3 color changed

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
+        <h3>git demo</h3>
         <?php
         // put your code here
         ?>
